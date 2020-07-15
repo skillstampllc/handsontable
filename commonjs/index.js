cba3ccfe18b0e1d480684235220e02994bde9066
Dmitriy Naumenko <naymwm@gmail.com>
2020-07-15 11:43:05 +0300

    feat: work with all data range formulas

--- commonjs/index.js
+++ commonjs/index.js
@@ -97,7 +97,7 @@ Handsontable._getListenersCounter = _eventManager.getListenersCounter; // For Me
 Handsontable._getRegisteredMapsCounter = _mapCollection.getRegisteredMapsCounter; // For MemoryLeak tests
 
 Handsontable.packageName = 'handsontable';
-Handsontable.buildDate = "14/07/2020 15:54:48";
+Handsontable.buildDate = "15/07/2020 11:39:38";
 Handsontable.version = "7.4.2"; // Export Hooks singleton
 
 Handsontable.hooks = _pluginHooks.default.getSingleton(); // TODO: Remove this exports after rewrite tests about this module
